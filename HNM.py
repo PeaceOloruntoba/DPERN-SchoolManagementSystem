9ba5bdfd6b091bd9e335edd36cce603fdddce8ff
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2023-06-01 01:23:09 +0100

    Create HNM.py

---
+++ HNM.py
@@ -0,0 +1,8 @@
+import datetime
+
+# Get the current date
+current_date = datetime.date.today()
+
+# Check if it's the first day of the month
+if current_date.day == 1:
+    print("Happy New Month!!!")
